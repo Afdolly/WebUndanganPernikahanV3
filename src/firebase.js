5acd3ca35cf3d9a94fff60fe435e6ed74151900d
Afdolly <m.z.afdolly@gmail.com>
2025-02-08 15:28:20 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 
 // Konfigurasi Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyASyAzk1UP-TPGQvClQdBVtQWAAoU0XbY8",
-  authDomain: "webundanganpernikahanv3.firebaseapp.com",
-  databaseURL: "https://webundanganpernikahanv3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "webundanganpernikahanv3",
-  storageBucket: "webundanganpernikahanv3.firebasestorage.app",
-  messagingSenderId: "647001803125",
-  appId: "1:647001803125:web:5d54cf5f6a9b08284bab3c",
-  measurementId: "G-ZQM4M6R84J"
+  apiKey: "YourAPIKey",
+  authDomain: "YourAuthDomain",
+  databaseURL: "YourDatabaseUrl",
+  projectId: "YourProjectID",
+  storageBucket: "YourStorageBucket",
+  messagingSenderId: "YourMessagingSenderID",
+  appId: "YourAPPID",
+  measurementId: "YourMeasurementID"
 };
 
 // Inisialisasi Firebase
